e70c718507288d22c4a1c8f860fdbccde0ef658e
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-01-30 07:03:43 +0100

    chore: more window params

--- src/maguic.cc
+++ src/maguic.cc
@@ -1,8 +1,14 @@
 #include "maguic.hh"
 #include <string>
 
+#include <SDL.h>
+
 namespace Maguic
 {
+    Window::Window(std::string name, int width, int height)
+    {
+        
+    }
     Window::Window(std::string name)
     {
     }
--- src/maguic.hh
+++ src/maguic.hh
@@ -1,4 +1,8 @@
+#pragma once
+
 #include <string>
+
+#include <SDL.h>
 
 namespace Maguic
 {
@@ -6,8 +10,11 @@ namespace Maguic
     {
     private:
         std::string name_;
+        SDL_Window *window_;
+        int width_, height_;
     public:
         Window(std::string name);
+        Window(std::string name, int width, int height);
         ~Window();
     };
     
