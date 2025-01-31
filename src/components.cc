e83f03efcf025a34d6085a27e2834313bbb1c3ca
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-02-01 00:03:01 +0100

    feat: components + panel + button structures

---
+++ src/components.cc
@@ -0,0 +1,57 @@
+#include "maguic.hh"
+#include <string>
+#include <thread>
+#include <iostream>
+#include <functional>
+
+#include <SDL2/SDL.h>
+
+namespace Maguic
+{
+    // Component
+    Component::Component(int w, int h) : _width(w), _height(h) {}
+    Component::~Component() = default;
+
+    int Component::getWidth() const { return _width; }
+    int Component::getHeight() const { return _height; }
+    void Component::setSize(int w, int h) { _width = w;  _height = h; }
+
+    // Button
+    Button::Button(int width, int height, const std::string &name) : Component(width, height), _name(name) {}
+    Button::Button(const std::string &name) : Button(-1,-1, name) {}
+
+    std::string Button::getName() const { return _name; }
+    void Button::setName(const std::string &name) { _name = name; }
+
+    void Button::render() const
+    {
+        std::cout << "Button [" << _name << "] of size (" << _width << ", " << _height << ")\n";
+    }
+
+    // Panel
+    Panel::Panel(int width, int height) : Component(width, height), _components() {}
+    Panel::Panel() : Panel(-1, -1) {}
+    Panel::~Panel()
+    {
+        for (std::shared_ptr<Maguic::Component> &comp : _components)
+        {
+            comp->~Component();
+        }
+    }
+    void Panel::removeAllComponents()
+    {
+        _components.empty();
+    }
+
+    void Panel::addComponent(std::shared_ptr<Component> component)
+    {
+        _components.push_back(component);
+    }
+
+    void Panel::render() const {
+    std::cout << "Panel of size (" << _width << ", " << _height << ") containing:\n";
+    for (const std::shared_ptr<Maguic::Component> &comp : _components) {
+        comp->render();
+    }
+}
+} // namespace Maguic
--- src/maguic.hh
+++ src/maguic.hh
@@ -4,6 +4,8 @@
 
 #include <string>
 #include <thread>
+#include <vector>
+#include <memory>
 
 #include <SDL2/SDL.h>
 
@@ -19,13 +21,65 @@ namespace Maguic
         bool _running;
 
         void mainWindowLoop();
+
     public:
         Window(std::string name, int width, int height);
         Window(std::string name);
         ~Window();
         void setVisible(bool visible);
     };
-    
+
+    // ##################### COMPONENTS ##################### //
+
+    // Abstract component
+    class Component
+    {
+    protected:
+        int _width, _height;
+
+    public:
+        Component(int w, int h) : _width(w), _height(h) {}
+
+        virtual ~Component() = default;
+
+        int getWidth() const;
+        int getHeight() const;
+
+        void setSize(int width, int height);
+
+        virtual void render() const = 0;
+    };
+
+    class Button : public Component
+    {
+    private:
+        std::string _name;
+
+    public:
+        Button(int width, int height, const std::string &name);
+        Button(const std::string &name);
+
+        std::string getName() const;
+        void setName(const std::string &name);
+
+        void render() const;
+    };
+
+    class Panel : public Component
+    {
+    protected:
+        std::vector<std::shared_ptr<Component>> _components;
+
+    public:
+        Panel();
+        Panel(int width, int height);
+
+        void removeAllComponents();
+        void addComponent(std::shared_ptr<Component> component);
+
+        void render() const;
+    };
+
 } // namespace Maguic
 
 #endif
